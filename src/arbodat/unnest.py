f67eaaf165f7007c765e169036250a18dab2ce7a
Roger Mähler <roger.mahler@hotmail.com>
2025-11-26 12:27:14 +0100

    refactor: allow id_vars and value_vars to be undefined

--- src/arbodat/unnest.py
+++ src/arbodat/unnest.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 import pandas as pd
 from loguru import logger
 
@@ -17,27 +19,30 @@ def unnest(entity: str, table: pd.DataFrame, table_cfg: TableConfig) -> pd.DataF
     value_name: str = unnest_config.value_name or "value"
 
     if value_name and value_name in table.columns:
-        logger.info(f"Entity '{entity}': is melted already, skipping unnesting")
+        logger.info(f"Unnesting {entity}: is melted already, skipping unnesting")
         return table
 
-    if not id_vars or not value_vars or not var_name or not value_name:
-        raise ValueError(f"Invalid unnest configuration for entity '{entity}': {unnest_config}")
+    if not var_name or not value_name:
+        raise ValueError(f"Unnesting {entity}: Invalid configuration: {unnest_config}")
 
-    if not all(col in table.columns for col in id_vars):
-        missing: list[str] = [col for col in id_vars if col not in table.columns]
-        raise ValueError(f"Cannot unnest entity '{entity}', missing id_vars columns: {missing}")
+    opts: dict[str, Any] = {
+        "var_name": var_name,
+        "value_name": value_name,
+    }
 
-    missing_value_vars: list[str] = [col for col in value_vars if col not in table.columns]
-    if len(missing_value_vars) > 0:
-        logger.info(f"Deferring unnesting of entity '{entity}', missing value_vars found: {missing_value_vars}")
-        return table
+    if id_vars:
+        if not all(col in table.columns for col in id_vars):
+            missing: list[str] = [col for col in id_vars if col not in table.columns]
+            raise ValueError(f"Cannot unnest entity '{entity}', missing id_vars columns: {missing}")
+        opts["id_vars"] = id_vars
 
-    table_unnested: pd.DataFrame = pd.melt(
-        table,
-        id_vars=id_vars,
-        value_vars=value_vars,
-        var_name=var_name,
-        value_name=value_name,
-    )
+    if not value_vars:
+        missing_value_vars: list[str] = [col for col in value_vars if col not in table.columns]
+        if len(missing_value_vars) > 0:
+            logger.info(f"Deferring unnesting of entity '{entity}', missing value_vars found: {missing_value_vars}")
+            return table
+        opts["value_vars"] = value_vars
+
+    table_unnested: pd.DataFrame = pd.melt(table, **opts)
 
     return table_unnested
