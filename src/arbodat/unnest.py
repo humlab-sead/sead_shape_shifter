870422c07ec1e5777d40955d77c6b2536d7a9090
Roger Mähler <roger.mahler@hotmail.com>
2025-11-29 22:35:14 +0100

    fix(unnest): correct logic for handling value_vars presence in unnest function

--- src/arbodat/unnest.py
+++ src/arbodat/unnest.py
@@ -36,7 +36,7 @@ def unnest(entity: str, table: pd.DataFrame, table_cfg: TableConfig) -> pd.DataF
             raise ValueError(f"Cannot unnest entity '{entity}', missing id_vars columns: {missing}")
         opts["id_vars"] = id_vars
 
-    if not value_vars:
+    if value_vars:
         missing_value_vars: list[str] = [col for col in value_vars if col not in table.columns]
         if len(missing_value_vars) > 0:
             logger.info(f"Deferring unnesting of entity '{entity}', missing value_vars found: {missing_value_vars}")
