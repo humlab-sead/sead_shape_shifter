b19768f108666a73763a9191bfac8b66e1f008e6
Roger Mähler <roger.mahler@hotmail.com>
2025-12-11 17:24:18 +0100

    refactor: consolidate UCanAccessSqlLoader initialization with DataSourceConfig

--- tests/test_ucanaccess_loader.py
+++ tests/test_ucanaccess_loader.py
@@ -5,27 +5,33 @@ import jpype
 import pandas as pd
 
 from src.loaders.database_loaders import UCanAccessSqlLoader
+from src.config_model import DataSourceConfig, TableConfig
 
 UCANACCESS_HOME = os.path.abspath("lib/ucanaccess")
 ARBODAT_DATA_MDB = os.path.abspath("./input/ArchBotDaten.mdb")
 ARBODAT_LOOKUP_MDB = os.path.abspath("./input/ArchBotStrukDat.mdb")
 
+DATA_SOURCE_CONFIG = DataSourceConfig(
+    cfg={
+        "options": {
+            "filename": ARBODAT_LOOKUP_MDB,
+            "ucanaccess_dir": UCANACCESS_HOME,
+        },
+        "driver": "ucanaccess",
+    },
+    name="ucanaccess_test",
+)
+
 
 def test_can_load_ucanaccess():
-    opts: dict[str, Any] = {
-        "filename": ARBODAT_LOOKUP_MDB,
-        "ucanaccess_dir": UCANACCESS_HOME,
-    }
-    loader = UCanAccessSqlLoader(opts)
-
+    loader = UCanAccessSqlLoader(DATA_SOURCE_CONFIG)
     df: pd.DataFrame = loader.read_sql_sync("SELECT * FROM [Kulturgruppen]")
-
     assert not df.empty
 
 
 def test_can_load_ucanaccess_queries() -> None:
 
-    loader: UCanAccessSqlLoader = UCanAccessSqlLoader()
+    loader: UCanAccessSqlLoader = UCanAccessSqlLoader(DATA_SOURCE_CONFIG)
     jar_files = loader._find_jar_files(UCANACCESS_HOME)  # pylint: disable=protected-access
 
     classpath = ":".join(jar_files)
@@ -35,7 +41,7 @@ def test_can_load_ucanaccess_queries() -> None:
         jpype.startJVM(jpype.getDefaultJVMPath(), "-ea", f"-Djava.class.path={classpath}")
     except OSError:
         pass
-    
+
     conn = jpype.java.sql.DriverManager.getConnection(f"jdbc:ucanaccess:///{filename}")
 
     for query in conn.getDbIO().getQueries():
