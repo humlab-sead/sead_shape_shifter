0b1d3d6db6e988f56c9e51c9a40c72b8f5384ade
Roger Mähler <roger.mahler@hotmail.com>
2025-12-11 17:23:09 +0100

    refactor: clean up Transformer and TransformerRegistry classes in transforms module

--- src/transforms/common.py
+++ src/transforms/common.py
@@ -1,32 +1,15 @@
 import abc
-from re import A
 from typing import Any
-from src.utility import Registry
 
 import pandas as pd
-
-
-class TransformerRegistry(Registry):
-
-    items: dict[str, type["Transformer"]] = {}
-
-
-Transformers: TransformerRegistry = TransformerRegistry()
-
-
-class Transformer(abc.ABC):
-
-    def apply(self, data: pd.DataFrame, columns: list[str] | str, **opts) -> pd.DataFrame:
-        return data
-
-
-Transformers.register(key="translate")
 
 
 class TranslateTransformer(abc.ABC):
     """Given a mapping dict, transform values in specified columns."""
 
-    def apply(self, data: pd.DataFrame, columns: list[str] | str, *, mapping: dict[Any, Any], **opts) -> pd.DataFrame:
+    def apply(
+        self, data: pd.DataFrame, columns: list[str] | str, *, mapping: dict[Any, Any], **opts  # pylint: disable=unused-argument
+    ) -> pd.DataFrame:
         df: pd.DataFrame = data.copy()
         for col in [columns] if isinstance(columns, str) else columns:
             df[col] = df[col].map(mapping).fillna(df[col])
