4b564c94b364e52303ed8170513d7091cc065abd
Roger Mähler <roger.mahler@hotmail.com>
2026-01-02 10:35:47 +0100

    feat: import  invoke DataLoaders.scan() Resolves #88

--- src/loaders/__init__.py
+++ src/loaders/__init__.py
@@ -1,4 +1,5 @@
 # type: ignore
 
-from . import exel_loaders, file_loaders, fixed_loader, sql_loaders  # noqa: E402, F401
 from .base_loader import DataLoader, DataLoaderRegistry, DataLoaders
+
+DataLoaders.scan()
