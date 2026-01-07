01b37d05dfa3fc489e872b5855d841587e966e4d
Roger Mähler <roger.mahler@hotmail.com>
2026-01-07 13:42:19 +0100

    fix: refactor output path handling to use Path objects and improve shape validation logic

--- tests/process/test_workflow.py
+++ tests/process/test_workflow.py
@@ -1,5 +1,6 @@
 import asyncio
 import os
+from pathlib import Path
 import shutil
 
 import pytest
@@ -89,18 +90,17 @@ def test_access_database_csv_workflow():
 
     translate: bool = False
 
-    output_path: str = "tmp/arbodat-test.xlsx"
+    output_path: Path = Path("tmp/arbodat-test.xlsx")
     asyncio.run(asyncio.sleep(0.1))  # type: ignore ; ensure config is fully loaded;
 
-    if os.path.exists(output_path):
-        shutil.rmtree(output_path, ignore_errors=True)
+    remove_path(output_path)
 
     assert not os.path.exists(output_path)
 
     _ = asyncio.run(
         workflow(
             project=config,
-            target=output_path,
+            target=str(output_path),
             translate=translate,
             target_type="xlsx",
             drop_foreign_keys=False,
@@ -108,22 +108,34 @@ def test_access_database_csv_workflow():
     )
 
     assert os.path.exists(output_path)
-    assert os.path.exists(os.path.join(output_path, "table_shapes.tsv"))
 
-    if not os.path.exists(output_path):
-        raise FileNotFoundError(f"Output path not found: {output_path}")
+    shape_filename: Path = (output_path if output_path.is_dir() else output_path.parent) / "table_shapes.tsv"
 
+    assert os.path.exists(shape_filename)
+
+
+def check_regression_of_shapes(output_path: Path, shape_filename: Path):
     # Load and verify table shapes
     # Truth is stored in ./input/table_shapes.tsv
     # We need to compare this against the generated tsv-files in output_path
-    truth_shapes: dict[str, tuple[int, int]] = load_shape_file(filename="./input/table_shapes.tsv")
-    new_shapes: dict[str, tuple[int, int]] = load_shape_file(filename=os.path.join(output_path, "table_shapes.tsv"))
+    truth_filename: Path = Path("./input/table_shapes.tsv")
+    if truth_filename.exists():
+        truth_shapes: dict[str, tuple[int, int]] = load_shape_file(filename=str(truth_filename))
+        new_shapes: dict[str, tuple[int, int]] = load_shape_file(filename=str(shape_filename))
 
-    entities_with_different_shapes = [  # noqa: F841 ;pylint: disable=unused-variable
-        (entity, truth_shapes.get(entity), new_shapes.get(entity))
-        for entity in set(truth_shapes.keys()).union(set(new_shapes.keys()))
-        if truth_shapes.get(entity) != new_shapes.get(entity)
-    ]
-    validate_entity_shapes(output_path, "csv", "./input/table_shapes.tsv")
+        entities_with_different_shapes = [  # noqa: F841 ;pylint: disable=unused-variable
+            (entity, truth_shapes.get(entity), new_shapes.get(entity))
+            for entity in set(truth_shapes.keys()).union(set(new_shapes.keys()))
+            if truth_shapes.get(entity) != new_shapes.get(entity)
+        ]
+        validate_entity_shapes(str(output_path), "csv", str(shape_filename))
+
+
+def remove_path(output_path):
+    if os.path.exists(output_path):
+        if os.path.isdir(output_path):
+            shutil.rmtree(output_path)
+        else:
+            os.remove(output_path)
 
     # assert len(entities_with_different_shapes) == 0, f"Entities with different shapes: {entities_with_different_shapes}"
