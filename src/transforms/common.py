78d50be71668e71b35c1c57db788973c6fecbeab
Roger Mähler <roger.mahler@hotmail.com>
2026-01-23 12:58:04 +0100

    refactor: fixed circular import by moving Transformer and TransformerRegistry classes to common module

--- src/transforms/common.py
+++ src/transforms/common.py
@@ -3,6 +3,29 @@ from typing import Any
 
 import pandas as pd
 
+from pathlib import Path
+
+from src.utility import Registry
+
+# pylint: disable=unused-argument
+
+
+class Transformer(abc.ABC):
+
+    def is_satisfied(self, data: pd.DataFrame, columns: list[str] | str, **opts) -> bool:
+        return True
+
+    def apply(self, data: pd.DataFrame, columns: list[str] | str, **opts) -> pd.DataFrame:
+        return data
+
+
+class TransformerRegistry(Registry):
+
+    items: dict[str, type["Transformer"]] = {}
+
+
+Transformers: TransformerRegistry = TransformerRegistry().scan(__name__, Path(__file__).parent)  # pylint: disable=invalid-name
+
 
 class TranslateTransformer(abc.ABC):
     """Given a mapping dict, transform values in specified columns."""
